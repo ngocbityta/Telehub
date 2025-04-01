f536cb5409c08a32bbd1d73bfcf95a43e7ed5a96
Dang Dinh Khai <oingucoolname@gmail.com>
2025-04-01 21:36:25 +0700

    Add files via upload

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import { Container, Nav, Navbar, Stack } from "react-bootstrap";
+import {Link} from "react-router-dom"
+import { AuthContext } from "../context/AuthContext";
+import { useContext, useEffect ,useState} from "react";
+import Notification from "./chats/Notification";
+import ChatBot  from "./chatbot/ChatBot";
+const NavBar = () => {
+
+    const {user, logoutUser} = useContext(AuthContext);
+    return ( <Navbar bg="dark" className="mb-4" style={{height:"3.75rem"}}>
+    <Container>
+        <h2>
+            {/* co the them anh */}
+            <Link to="/" className="link-light text-decoration-none">Chat P2P</Link>
+        </h2>
+        {user &&<span className="text-warning">
+        Đã đăng nhập
+        {/* as {user?.name} */}
+        </span>}
+        <Nav>
+            <Stack direction="horizontal" gap={3}>
+            {
+                user &&(<>
+                    <ChatBot/>
+                    <Notification/>
+                    <Link to="/profile" className="profile link-light text-decoration-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                    </svg>
+                    </Link>
+                    <Link onClick={() => logoutUser()} to="/login" className="link-light text-decoration-none">Đăng xuất</Link>
+                </>)
+            }
+            {
+                !user && (<>
+                <Link to="/login" className="link-light text-decoration-none">Đăng nhập</Link>
+                <Link to="/register" className="link-light text-decoration-none">Đăng ký</Link>
+                </>
+                )
+            }
+            
+            </Stack>
+        </Nav>
+    </Container>
+    </Navbar> );
+};
+ 
+export default NavBar;
