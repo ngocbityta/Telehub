7345ab87d099936e76ad50f56936db26cce1d7c6
Khai Tran <tranmanhkhai2004@gmail.com>
2025-05-18 20:07:47 +0700

    fix: No more lint for FE

--- client/src/pages/home/Groups.js
+++ client/src/pages/home/Groups.js
@@ -1,36 +1,33 @@
-import CreateBox from '../../components/grouppanel/CreateBox';
-import GroupList from '../../components/grouppanel/GroupList';
-import useUserData from '../../hooks/useUserData';
+import CreateBox from "../../components/grouppanel/CreateBox";
+import GroupList from "../../components/grouppanel/GroupList";
+import useUserData from "../../hooks/useUserData";
 
 const Groups = () => {
-
   const { listGroup, setListGroup } = useUserData();
 
   const handleCreateGroup = async (newGroup) => {
     setListGroup((prev) => {
       return [...prev, newGroup];
     });
-  }
+  };
 
   return (
-    <div className="h-auto min-w-[350px] bg-[var(--page-bg)]" >
+    <div className="h-auto min-w-[350px] bg-[var(--page-bg)]">
       {/* Header */}
       <div>
-        <h1 className='font-bold text-blue-500 text-2xl p-3'> Groups </h1>
+        <h1 className="font-bold text-blue-500 text-2xl p-3"> Groups </h1>
       </div>
 
       {/* Create Button */}
-      <div className='p-3'>
+      <div className="p-3">
         <CreateBox onCreateGroup={handleCreateGroup} />
 
-        <div className='pt-6'>
+        <div className="pt-6">
           <GroupList listGroup={listGroup} setListGroup={setListGroup} />
         </div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Groups;
