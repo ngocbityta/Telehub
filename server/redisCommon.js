294d3d47cbfd08be5a6bbec999cd6564bfc757fa
ƠncủaKhải <42139584+ngocbityta@users.noreply.github.com>
2025-05-22 15:07:30 +0700

    feat: add redis (#11)

---
+++ server/redisCommon.js
@@ -0,0 +1,37 @@
+import redis from "redis";
+let client = null;
+
+let connected = false;
+const _setupRedis = async () => {
+  console.log(`connect to redis://localhost:8000/6379...`);
+  client = redis.createClient({
+    url: `redis://localhost:8000/6379`,
+    socket: {
+      reconnectStrategy: (retries) => {
+        if (retries > 20) {
+          return new Error("Too many retries for redis connection");
+        }
+        return retries * 500;
+      },
+      connectTimeout: 10000, // timeout after 10s
+    },
+  });
+  client.on("error", function (err) {
+    const message = `error connectting redis. ${err.stack}`;
+    console.log(message);
+    connected = false;
+  });
+  client.on("ready", () => {
+    console.log("connected to redis and ready");
+    connected = true;
+  });
+  client.connect().then(() => console.log("start connect to redis"));
+};
+
+_setupRedis();
+
+const isRedisConnected = () => {
+  return connected;
+};
+
+export { client, isRedisConnected };
