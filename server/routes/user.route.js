301c776ebda98d41079fedcc4f832971f480633e
Ubuntu <ubuntu@ip-172-31-13-201.ap-southeast-2.compute.internal>
2025-06-01 14:25:09 +0000

    fix: name

---
+++ server/routes/user.route.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import multer from "multer";
+import userController from "../Controllers/user.controller.js";
+
+const router = Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/edit", upload.single("image"), userController.handleEditInfo);
+router.post("/change-password", userController.handleChangePassword);
+router.get("/:userId", userController.getUserById);
+router.post("/get-users", userController.getUsers);
+
+export default router;
